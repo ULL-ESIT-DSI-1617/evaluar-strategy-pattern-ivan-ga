3eb55b3341f10b431fce181c7010b81c232f4cea
Ivan Gonzalez <alu0100551266@ull.edu.es>
2017-04-19 14:35:23 +0100

    cambiando tests

--- test/test.js
+++ test/test.js
@@ -5,19 +5,23 @@ describe("Cálculo de áreas", function() {
   it("must compute the triangle area correctly", function() {
     var result = index.calcula('Triangle',  { width: 100, height: 100 });
     /* There is a white space between consecutive columns */
-    result.should.match(/^5000$/);
+    //result.should.match(/^5000$/);
+    result.should.be.equal(5000)
   })
 
   it("must compute the Rectangle area correctly", function() {
-    var result = index.calcula('Triangle',  { width: 10 });
+    var result = index.calcula('Rectangle',  { width: 2, height: 4 });
     /* There is a white space between consecutive columns */
-    result.should.match(/^100$/);
+    console.log(result)
+    result.should.be.equal(8)
+    //result.should.match(/^100$/);
   })
 
   it("must compute the Square area correctly", function() {
-    var result = index.calcula('Triangle',  { width: 10, height: 2 });
+    var result = index.calcula('Square',  { width: 3 });
     /* There is a white space between consecutive columns */
-    result.should.match(/^20$/);
+    //result.should.match(/^2$/);
+    result.should.be.equal(9)
   })
 
 });
