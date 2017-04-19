41eda130d6bd503b0bdce3b2dad9f48e83e2349e
Ivan Gonzalez <alu0100551266@ull.edu.es>
2017-04-19 14:18:03 +0100

    añadiendo travis

--- test/test.js
+++ test/test.js
@@ -1,23 +1,19 @@
 var getArea = require("../shapesarea.js");
 var index = require("../index.js");
 
-describe("area triangle", function() {
+describe("Cálculo de áreas", function() {
   it("must compute the triangle area correctly", function() {
     var result = index.calcula('Triangle',  { width: 100, height: 100 });
     /* There is a white space between consecutive columns */
     result.should.match(/^5000$/);
   })
-});
 
-describe("area rectangle", function() {
   it("must compute the Rectangle area correctly", function() {
     var result = index.calcula('Triangle',  { width: 10 });
     /* There is a white space between consecutive columns */
     result.should.match(/^100$/);
   })
-});
 
-describe("area square", function() {
   it("must compute the Square area correctly", function() {
     var result = index.calcula('Triangle',  { width: 10, height: 2 });
     /* There is a white space between consecutive columns */
